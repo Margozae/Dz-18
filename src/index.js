c3bed1faeec46f9131fa7373a6bbbe907e17397f
Margozae <59208589+Margozae@users.noreply.github.com>
2020-04-07 09:39:53 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,15 @@ data.then(item => item.json())
         const divFilm = document.createElement('div');
         container.append(divFilm);
         divFilm.append(result.results[j].title);
-        
         function getShip() {
             const dataNew = fetch('https://swapi.co/api/films/');
             dataNew.then(item => item.json())
-            .then(result => { for (let i=0; i < result.results[j].starships.length; i++ ) { 
-                let divShip = document.createElement('div');
-                divFilm.append(divShip);
-                fetch(`${result.results[j].starships[i]}`)
-                .then(item => item.json())
-                .then(result => divShip.append(result.name))
+                .then(result => { for (let i=0; i < result.results[j].starships.length; i++ ) { 
+                    let divShip = document.createElement('div');
+                    divFilm.append(divShip);
+                    fetch(`${result.results[j].starships[i]}`)
+                    .then(item => item.json())
+                    .then(result => divShip.append(result.name))
                 }
                 })
             divFilm.removeEventListener('click', getShip);
@@ -24,8 +23,6 @@ data.then(item => item.json())
         divFilm.addEventListener('click', getShip);
     }
     });
-
-
 
 
 
